9b57c9d2409a0b9249102c4a833fb75f5b425133
jngsoo <wt2933@icloud.com>
2019-10-10 12:20:56 +0900

    Feature : delete column done
    관련 Tasks 들도 cascade 삭제

--- model/project.js
+++ model/project.js
@@ -10,16 +10,14 @@ module.exports = {
                             results.forEach(lane => {
                                 lanes.push(`'${lane.id}'`)
                             })
-                            pool.query(`DELETE FROM projects WHERE id = '${project_id}'`)
-                        }
+                            pool.query(`DELETE FROM projects WHERE id = '${project_id}'`)}
         )
     },
     editLaneTitle(lane_id, title) {
         pool.query(`UPDATE lanes SET name='${title}' WHERE id = '${lane_id}';`)
     },
     removeLane(lane_id) {
-        pool.query()
-        //cascade query!
+        pool.query(`DELETE FROM lanes WHERE id = '${lane_id}';`)
     },
     removeTask(task_id) {
         pool.query(`DELETE FROM tasks WHERE id = '${task_id}';`)
